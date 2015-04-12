d80e5b5795d7b250fc075e3f7d97e26962b2315c
Eric Hagan <ehagan@gmail.com>
2015-04-12 16:51:58 -0500

    Fourth plot script. Copies from plot2.R and plot3.R rather than source them in some clever way.

---
+++ plot4.R
@@ -0,0 +1,45 @@
+# This should be in a separate file like "loadData.R" which you could
+# then do source("loadData.R") on. But, I'm going to leave it as boilerplate
+# in each function for grading, working directory simplification and so on.
+loadData <- function() {
+    if (! file.exists("household_power_consumption.txt")) {
+        if (! file.exists("exdata-data-household_power_consumption.zip")) {
+            download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
+                          "exdata-data-household_power_consumption.zip")
+        }
+        unzip("exdata-data-household_power_consumption.zip")
+        if (! file.exists("household_power_consumption.txt")) {
+            stop("Cannot locate or acquire household_power_consumption.txt")
+        }
+    }
+    
+    
+    # Read enough data to get what we want without reading the whole 2MM line file
+    data <- read.table("household_power_consumption.txt",sep=";",na.strings="?",header=T,nrows=75000)
+    data$DateTime = as.POSIXct(paste(data$Date, data$Time), format="%d/%m/%Y %H:%M:%S")
+    
+    startTm = strptime("2007-02-01 00:00:00","%F %T")
+    endTm   = strptime("2007-02-03 00:00:00","%F %T")
+    
+    subData = subset(data, ((DateTime >= startTm) & (DateTime < endTm)))
+    subData
+}
+
+d <- loadData()
+
+# Create grid
+par(mfrow=c(2,2))
+
+# First plot - copied from plot2.R
+with(d, plot(DateTime, Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)", main="" ))
+
+# Second plot
+with(d, plot(DateTime, Voltage, type="l", xlab="datetime", ylab="Voltage", main=""))
+
+# Third plot - copied from plot3.R
+with(d, plot(DateTime, Sub_metering_1, xlab="", ylab="Energy sub metering", type="l", col="black", main=""))
+with(d, lines(DateTime, Sub_metering_2, xlab="", ylab="Energy sub metering", type="l", col="red", main=""))
+with(d, lines(DateTime, Sub_metering_3, xlab="", ylab="Energy sub metering", type="l", col="blue", main=""))
+with(d, legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),lty=c(1,1,1), col=c("black","red","blue"), bty="n"))
+
+with(d, plot(DateTime, Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power", main=""))
